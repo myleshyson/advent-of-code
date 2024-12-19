34b9df6b78d21f74f46789e8021e156715f1e53b
Myles Hyson <myleshyson@ufl.edu>
2024-12-19 12:41:44 -0800

    Had to watch another yt video. starting to hit my limit.

---
+++ 2024/19.cpp
@@ -0,0 +1,62 @@
+#include <fstream>
+#include <sstream>
+#include <unordered_set>
+#include <unordered_map>
+
+#include "helpers.h"
+long long possibilities(std::string design, int &maxLength, std::unordered_set<std::string> &towels, std::unordered_map<std::string, long long> &cache) {
+    if (cache.contains(design)) return cache[design];
+
+    if (design.empty()) return 1;
+
+    long long count = 0;
+
+    for (int i = 0; i < std::min(maxLength, static_cast<int>(design.size())); i++) {
+        if (towels.contains(design.substr(0, i + 1))) {
+            count += possibilities(design.substr(i + 1), maxLength, towels, cache);
+        }
+    }
+
+    cache[design] = count;
+
+    return count;
+}
+
+int main() {
+    std::ifstream input = getStream("19.txt");
+    std::string content((std::istreambuf_iterator(input)), std::istreambuf_iterator<char>());
+    auto splitPos = content.find("\n\n");
+    auto towelString = content.substr(0, splitPos);
+    auto designs = content.substr(splitPos + 2);
+    std::stringstream towelStream(towelString);
+    std::string towel;
+    std::unordered_set<std::string> towels;
+    int maxLength = 0;
+
+    while (getline(towelStream, towel, ',')) {
+        std::string temp;
+
+        if (towel.at(0) == ' ') {
+            temp = towel.substr(1);
+        } else {
+            temp = towel;
+        }
+
+        maxLength = std::max(maxLength, static_cast<int>(temp.size()));
+        towels.insert(temp);
+    }
+
+    std::stringstream designStream(designs);
+    std::string design;
+
+    long long answer[] = {0, 0};
+
+    while (getline(designStream, design, '\n')) {
+        std::unordered_map<std::string, long long> cache;
+        long long count = possibilities(design, maxLength, towels, cache);
+        answer[0] += count > 0 ? 1 : 0;
+        answer[1] += count;
+    }
+
+    std::println("{}", answer);
+}
